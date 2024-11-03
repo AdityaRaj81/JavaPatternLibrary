5ff458faa96a1ed3dc5b02dda6642d530f61e6ee
AdityaRaj81 <Connect2RajAditya@gmail.com>
2024-11-03 16:52:08 +0530

    Implement pyramid star pattern in Pattern5.java

--- Pattern5.java
+++ Pattern5.java
@@ -1,6 +1,19 @@
-class solution 
-{
+class solution{
+
   public static void main(String[] args) {
-    System.out.println("Aditya");
+    int rows = 5; // Number of rows for the pyramid
+
+    for (int i = 1; i <= rows; i++) {
+      // Print spaces
+      for (int j = rows; j > i; j--) {
+        System.out.print(" ");
+      }
+      // Print stars
+      for (int k = 1; k <= (2 * i - 1); k++) {
+        System.out.print("*");
+      }
+      // Move to the next line
+      System.out.println();
+    }
   }
 }
