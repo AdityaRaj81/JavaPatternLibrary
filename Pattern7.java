252861b0ef3c5f68b6f00c956131339f7e5864cf
AdityaRaj81 <Connect2RajAditya@gmail.com>
2024-11-17 23:21:10 +0530

    pattern & added

---
+++ Pattern7.java
@@ -0,0 +1,42 @@
+/*
+ * Pattern Name: Hollow Diamond Pattern
+ */
+public class Pattern7 {
+    public static void main(String[] args) {
+        int n = 5; // Size of the diamond
+        
+        // Upper half of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+        // Lower half of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
