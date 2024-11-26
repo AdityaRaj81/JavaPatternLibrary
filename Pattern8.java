791124ea938057ea8c331325e8c912a1782e2248
AdityaRaj81 <Connect2RajAditya@gmail.com>
2024-11-26 19:06:43 +0530

    Add Pattern8.java to implement inverted pyramid star pattern

---
+++ Pattern8.java
@@ -0,0 +1,20 @@
+public class Pattern8 {
+  public static void main(String[] args) {
+    int rows = 5;
+
+    // Upper part of the pyramid
+    for (int i = rows; i >= 1; i--) {
+        // Print leading spaces
+        for (int j = rows - i; j > 0; j--) {
+            System.out.print(" ");
+        }
+
+        // Print stars
+        for (int k = 2 * i - 1; k > 0; k--) {
+            System.out.print("*");
+        }
+
+        System.out.println(); // Move to next line
+    }
+}
+}
